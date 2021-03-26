df3add51c135f2327a335a2a65f668a5e5b6f8b3
mcorengia1 <mcorengia1@gmail.com>
2021-03-26 13:50:56 -0300

    Carga dinamica de categorias en index.html

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-var server = app.listen(3000, function(){
+var server = app.listen(3000, function() {
     //Codigo que se ejecuta cada vez que se levanta el server
     console.log('Servidor ejecutandose correctamente');
 })
@@ -9,8 +9,7 @@ var server = app.listen(3000, function(){
 //Cuando no encuentre archivos estaticos los va a buscar en la ruta que le diga
 app.use(express.static(__dirname));
 
-app.get('/',  function(req, res){
+app.get('/', function(req, res) {
     //Codigo que se ejecuta cada vez que se accese a '/'
-    res.sendFile( __dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 })
-
--- js/app.js
+++ js/app.js
@@ -1,13 +1,63 @@
-const { json } = require('express');
-const fs = require('fs');
-const { parse } = require('path');
+"use strict";
 
-var categories = fs.readFile('../json/categories.json', function(error) {
-    if (error) {
-        console.log(error);
-        throw ('Error de lectura del archivo de categorias, existe el archivo?')
-    } else {
-        console.log('Lectura correcta');
-        console.log(JSON.parse(categories));
-    }
-});
+/*
+
+Este modulo es el encargado de leer las categorias y cargarlas en la web index.html
+
+*/
+
+fetch('/json/categories.json')
+    .then(response => response.json())
+    .then(data => loadCategories(data));
+//Obtengo los datos desde el archivo JSON, lo convierto a objeto
+//Luego lo uso para crear los objetos en el HTML
+
+//Cargo las categorias en index.html
+function loadCategories(categories) {
+    categories.categories.forEach(category => {
+        //En el JSON en la primer propiedad pongo las categorias que va a tener guardadas
+        //Luego defino la propiedad de cada categoria cada una con un array de valores
+
+        let categoriesDiv = document.createElement('div');
+        let categoriesName = document.createElement('p');
+        let productsSearch = document.getElementById('products_search');
+        let categoriesCount;
+        //Creo los elementos por cada categoria principal
+
+        categoriesName.innerHTML = category;
+        categoriesDiv.className = 'categories';
+        //Asigno los valores a la categoria principal y su respectiva clase
+
+        productsSearch.appendChild(categoriesName);
+        productsSearch.appendChild(categoriesDiv);
+        //Creo los elementos html
+
+        categoriesCount = productsSearch.children.length;
+        //Guardo la cantidad de elementos que tiene la capa de busqueda
+        //Para poder agregar las subcategorias siempre en la ultima capa añadida
+
+        console.log(categoriesCount);
+
+        for (let index in categories[category]) {
+            console.log(categories[category][index]);
+            console.log(productsSearch.children[categoriesCount - 1]);
+
+            let categoryDiv = document.createElement('div');
+            let categoryA = document.createElement('a');
+            let subCategoriesCount;
+
+            categoryDiv.className = 'category';
+            categoryA.href = '/' + categories[category][index];
+            categoryA.text = categories[category][index];
+
+            productsSearch.children[categoriesCount - 1].appendChild(categoryDiv);
+            //Obtengo la ultima categoria y ahi le agrego la subcategoria
+
+            productsSearch.children[categoriesCount - 1].children[index].appendChild(categoryA);
+            //En orden accedo a la ultima categoria, luego a la subCategoria actual y ahi inserto el link
+
+        }
+
+    });
+
+}
